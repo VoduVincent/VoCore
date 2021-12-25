4f782c30bca5cd65047a22cb0868faa43b0eb194
unknown <Vodumapping@gmail.com>
2021-12-25 19:37:22 +0100

    I realized the MoreCore files where not neccesary at all

--- common/scripts/char_skillfilter.lua
+++
@@ -1,9 +0,0 @@
--- 
--- Please see the license.html file included with this distribution for 
--- attribution and copyright information.
---
-function onFilter(w)
--- Debug.console("char_skills_list.lua","onFilter","w",w);  
-  return w.getFilter();
-end
-
